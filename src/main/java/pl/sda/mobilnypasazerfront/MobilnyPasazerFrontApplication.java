3f0c55c4ca9624864a71a75f0dd6e679fea17c17
Ewa Raczynska-Bulawa <tanne@autograf.pl>
2019-06-22 10:51:39 +0200

    Init

---
+++ src/main/java/pl/sda/mobilnypasazerfront/MobilnyPasazerFrontApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.mobilnypasazerfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilnyPasazerFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( MobilnyPasazerFrontApplication.class, args );
+    }
+
+}
