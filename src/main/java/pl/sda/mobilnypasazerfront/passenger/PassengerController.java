3d0cd6a49dea04964d339b56e1214b6c92a9ded8
Ewa Raczynska-Bulawa <tanne@autograf.pl>
2019-06-22 15:53:09 +0200

    Widok formularza - koniec

--- src/main/java/pl/sda/mobilnypasazerfront/passenger/PassengerController.java
+++ src/main/java/pl/sda/mobilnypasazerfront/passenger/PassengerController.java
@@ -1,12 +1,12 @@
 package pl.sda.mobilnypasazerfront.passenger;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PassengerController {
 
-    @RequestMapping("/new-form")
+    @GetMapping("/new-form")
     String passengerForm () {
         return "passengerForm";
     }
