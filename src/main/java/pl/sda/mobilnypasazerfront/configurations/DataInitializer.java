3b8206f57b9bf21d7ff48b0335511fb52d763966
Aneta <aneta.plutek@gmail.com>
2019-06-30 10:26:36 +0200

    poprawny insert testowych danych do tabel

--- src/main/java/pl/sda/mobilnypasazerfront/configurations/DataInitializer.java
+++ src/main/java/pl/sda/mobilnypasazerfront/configurations/DataInitializer.java
@@ -13,32 +13,32 @@
 //@Component
 //public class DataInitializer implements InitializingBean {
 //
-////    @Autowired
-////    private RoleRepository roleRepository;
-////
-////    @Autowired
-////    private PassengerRepository passengerRepository;
-////
-////    @Autowired
-////    private PasswordEncoder passwordEncoder;
-////
-////    @Override
-////    public void afterPropertiesSet() throws Exception {
-////        if (roleRepository.count() == 0) {
-////            Role role_user = roleRepository.save(new Role("ROLE_USER"));
-////            Role role_admin = roleRepository.save(new Role("ROLE_ADMIN"));
-////            Passenger passengerAdmin = Passenger.builder()
-////                    .username("admin@pl.pl")
-////                    .passwordHash(passwordEncoder.encode("admin"))
-////                    .roles(Sets.newHashSet(role_admin))
-////                    .build();
-////            passengerRepository.save(passengerAdmin);
-////            Passenger passenger = Passenger.builder()
-////                    .username("user@pl.pl")
-////                    .passwordHash(passwordEncoder.encode("user"))
-////                    .roles(Sets.newHashSet(role_user))
-////                    .build();
-////            passengerRepository.save(passenger);
-////        }
-////    }
+//    @Autowired
+//    private RoleRepository roleRepository;
+//
+//    @Autowired
+//    private PassengerRepository passengerRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        if (roleRepository.count() == 0) {
+//            Role role_user = roleRepository.save(new Role("ROLE_USER"));
+//            Role role_admin = roleRepository.save(new Role("ROLE_ADMIN"));
+//            Passenger passengerAdmin = Passenger.builder()
+//                    .username("admin@pl.pl")
+//                    .passwordHash(passwordEncoder.encode("admin"))
+//                    .roles(Sets.newHashSet(role_admin))
+//                    .build();
+//            passengerRepository.save(passengerAdmin);
+//            Passenger passenger = Passenger.builder()
+//                    .username("user@pl.pl")
+//                    .passwordHash(passwordEncoder.encode("user"))
+//                    .roles(Sets.newHashSet(role_user))
+//                    .build();
+//            passengerRepository.save(passenger);
+//        }
+//    }
 //}
--- src/main/resources/db/migration/V2__populateTableInitialData.sql
+++ src/main/resources/db/migration/V2__populateTableInitialData.sql
@@ -1,8 +1,16 @@
 INSERT INTO roles (id, role_name)
-VALUES(1, 'ROLE_USER');
+VALUES((SELECT nextval('hibernate_sequence')), 'ROLE_USER');
 
 INSERT INTO roles (id, role_name)
-VALUES(2, 'ROLE_ADMIN');
+VALUES((SELECT nextval('hibernate_sequence')), 'ROLE_ADMIN');
 
 INSERT INTO public.passengers (id, first_name, last_name, password_hash, phone, sex, username)
-VALUES (1, 'test', 'user', '$2a$10$jGhgY09ndXO/YyVTucF2MurS4DIcLCuKaRYHH6f/GWy4hrXTst8eS', '12345678912', 0, 'Test@test.pl');
+VALUES ((SELECT nextval('hibernate_sequence')), 'test', 'user', '$2a$10$jGhgY09ndXO/YyVTucF2MurS4DIcLCuKaRYHH6f/GWy4hrXTst8eS', '12345678912', 0, 'Test@test.pl');
+
+INSERT INTO PUBLIC.PASSENGERS_ROLES (PASSENGER_ID, ROLES_ID)
+VALUES ((SELECT id
+          FROM public.passengers
+          WHERE username='Test@test.pl'),
+        (SELECT id
+          FROM roles
+          WHERE role_name='ROLE_USER'));
